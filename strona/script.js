783590ec7ec29ba2f4582abfac80c8340809361a
Glab01 <wojteklejk5@gmail.com>
2024-11-02 15:22:21 +0100

    not much

---
+++ strona/script.js
@@ -0,0 +1,16 @@
+function updateClock() {
+    const now = new Date();
+    
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+  
+    const timeStr = `${hours}:${minutes}:${seconds}`;
+  
+    document.getElementById('footer').textContent = timeStr;
+  }
+  
+  setInterval(updateClock, 1000);
+  
+  updateClock();
+  
