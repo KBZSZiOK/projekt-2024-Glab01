191573ff877119379031be07f1f7fef55163244f
Wojciech Lejk <wojteklejk5@gmail.com>
2024-11-07 13:49:38 +0100

    sd

--- strona/display.php
+++ strona/display.php
@@ -18,25 +18,19 @@
     } else {
         echo "Connection established <br>";
     }
-    $MovieQuery = "SELECT TYTUŁ, REŻYSER, CZAS_TRWANIA FROM FILMY;";
-    $MovieExec = $mysqli->query($MovieQuery);
-    
-    if ($MovieExec->num_rows > 0) {
 
-        echo "<br> <table border='1' cellpadding='5' cellspacing='0'>";
-        echo "<tr><th>Tytuł</th><th>Reżyser</th><th>Czas Trwania (min.)</th></tr>";
-    
-        while($row = $MovieExec->fetch_assoc()) {
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($row["TYTUŁ"]) . "</td>";
-            echo "<td>" . htmlspecialchars($row["REŻYSER"]) . "</td>";
-            echo "<td>" . htmlspecialchars($row["CZAS_TRWANIA"]) . "</td>";
-            echo "</tr>";
-        }
-    
-        echo "</table>";
-    } else {
-        echo "0 results";
+    $Query = "SELECT SEANSE.TERMIN, SEANSE.LICZBA_WOLNYCH_MIEJSC, FILMY.TYTUŁ FROM `SEANSE`, `FILMY` WHERE SEANSE.FILM_ID = FILMY.ID;";
+    $QueryExec = $mysqli->query($Query);
+
+    echo "<table border='1'>";
+    echo "<tr><th>Termin</th><th>Liczba wolnych miejsc</th><th>Tytuł filmu</th></tr>";
+    while ($row = $QueryExec->fetch_assoc()) {
+
+        echo "<tr>";
+        echo "<td>" . $row["TERMIN"] . "</td>";
+        echo "<td>" . $row['LICZBA_WOLNYCH_MIEJSC'] . "</td>";
+        echo "<td>" . $row['TYTUŁ'] . "</td>";
+        echo "</tr>";
     }
-    echo "asdasdad";
+    echo "</table>";
 ?>
